0b314c906eaa6f23611ac39385fb9240c6d64141
Eliza <doyoulikewafels@gmail.com>
2018-06-06 11:33:59 +0200

    data inladen

--- dataladen.js
+++ dataladen.js
@@ -7,36 +7,26 @@ $.getJSON( "namen.json", function( data ) {
     console.log("dit is de value: " + val.name);
     
     items.push( "<input id=\"" + val.id + "\"onclick=\"openPopup();\" type=\"image\" src=\"" + val.image + "\" name=\"" + val.name + "\">");
-    console.log("dit is de key: " + key);
-    console.log("dit is de value: " + val.name);
-      
-    console.log("this is the id: " + val.id);
-        
-    //items.push("<img class=\"userIcon\" src=\"" + val.image +"\" >")
-    //document.getElementById("Iconuser").innerHTML = val.image;
-//    document.getElementById("nameUser").innerHTML = val.name;
-//      document.getElementById("yearUser").innerHTML = val.year;
-//      document.getElementById("profileUser").innerHTML = val.profile;
-//      document.getElementById("specialityUser").innerHTML = val.speciality;
+
   });
  //$("#namenlijst").append(items);
  $("#class209").append(items);
 });
 
 $.getJSON( "namen.json", function( data ) {
+   // var item = | jq '.DATA | .[2]'
   var items = [];
   $.each( data, function( key, val ) {
       
       console.log("this is the id: " + val.id);
         
-      items.push("<img class=\"userIcon\" src=\"" + val.image +"\" >")
-      document.getElementById("Iconuser").innerHTML = val.image;
-      document.getElementById("nameUser").innerHTML = val.name;
-      document.getElementById("yearUser").innerHTML = val.year;
-      document.getElementById("profileUser").innerHTML = val.profile;
-      document.getElementById("specialityUser").innerHTML = val.speciality;
-  
+      items.push("<img class=\"userIcon\" src=\"" + val.image +"\" >");
+      items.push("<h2 style=\"text-align: center;\">" + val.name + "</h2>");
+      items.push("<h2>Year: " + val.year + "</h2>");
+      items.push("<h2>Profile: " + val.profile + "</h2>");
+      items.push("<h2>Specialty: " + val.specialty + "</h2>");
+      items.push("<h2>Specialization: " + val.specialization + "</h2>");
   });
-    $("#IconUser").append(items);
+    $("#userInfo").append(items);
 });
     
