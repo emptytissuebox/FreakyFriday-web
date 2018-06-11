5fdb4f94f7c54a541db63a8ca8fa8b6f0f04ce8c
Eliza <doyoulikewafels@gmail.com>
2018-06-11 10:25:45 +0200

    labels put on the right spot

--- dataladen.js
+++ dataladen.js
@@ -1,32 +1,37 @@
 $.getJSON( "namen.json", function( data ) {
   var items = [];
-  $.each( data, function( key, val ) {
-    
-//    items.push( "<li class=" + val.gender + " >" + val.name + "</li>" );
+  $.each( data.students, function( key, val ) {
+ 
     console.log("dit is de key: " + key);
     console.log("dit is de value: " + val.name);
     
-    items.push( "<input id=\"" + val.id + "\"onclick=\"openPopup();\" type=\"image\" src=\"" + val.image + "\" name=\"" + val.name + "\">");
-
+    items.push( "<input id=\"" + val.key + "\"onclick=\"openPopup();\" type=\"image\" src=\"" + val.image + "\" name=\"" + val.name + "\">");
+   
   });
  //$("#namenlijst").append(items);
  $("#class209").append(items);
 });
 
+function getKey() {
+    
+}
+
 $.getJSON( "namen.json", function( data ) {
-   // var item = | jq '.DATA | .[2]'
   var items = [];
-  $.each( data, function( key, val ) {
+  $.each( data.students, function( key, val ) {
       
       console.log("this is the id: " + val.id);
         
+      if (key == 0){
       items.push("<img class=\"userIcon\" src=\"" + val.image +"\" >");
+      
       items.push("<h2 style=\"text-align: center;\">" + val.name + "</h2>");
       items.push("<h2>Year: " + val.year + "</h2>");
       items.push("<h2>Profile: " + val.profile + "</h2>");
       items.push("<h2>Specialty: " + val.specialty + "</h2>");
-      items.push("<h2>Specialization: " + val.specialization + "</h2>");
+      items.push("<h2>Specialization: " + val.specialization + "</h2>");  
+      }
   });
-    $("#userInfo").append(items);
+    $("#IconUser").append(items);
 });
-    
+
